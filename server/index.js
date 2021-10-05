f1551fdb540586915bae94217f22f3454845198d
thomas <tmeyerwebdev@gmail.com>
2021-10-05 04:18:46 +0000

    cleaned up index and add some tweets

--- server/index.js
+++ server/index.js
@@ -32,6 +32,11 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
 
+
+
+
+
+
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
