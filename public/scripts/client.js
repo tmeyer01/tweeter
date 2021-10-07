9cca082a1dd30a26620b8d298d822f5b480d442c
thomas <tmeyerwebdev@gmail.com>
2021-10-07 00:56:12 +0000

    Add validation to make sure tweet wasnt over char count or empty string

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,42 +7,38 @@
 
 $(document).ready(function() {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-  
+
+  //Event listener for sumbit button when posting tweet
   $("form").on("submit", function(event) {
     event.preventDefault();
-    const tweet = $(this).serialize();
-    $.ajax({
-      type: "POST",
-      url: "/tweets/",
-      datatype: 'json',
-      data: tweet,
-    });
+    
+    //defining the count from the value entered in from text area with the id of tweet-text
+    let count = $("#tweet-text").val().length
+    
+    //validation to make sure count is not over 140, or no characters
+    if(count > 140) {
+      alert("Character count must be less then 140 character");
+      return;
+    } 
+    if (count === 0) {
+      alert("Cant not submit blank tweeter");
+      return;
+    } 
+
+      const tweet = $(this).serialize();
+      console.log(tweet);
+      $.ajax({
+        type: "POST",
+        url: "/tweets/",
+        datatype: 'json',
+        data: tweet,
+      });
+    
   });
 
+  
+
+  //Function creates new tweet-container and fills it with approrate data from data array object
   const createTweetElement = function(tweetObj) {
     let time = timeago.format(`${tweetObj.created_at}`);
 
@@ -70,25 +66,30 @@ $(document).ready(function() {
     );
     return $tweet;
   };
-
+  //fuction that filter through array of tweets and for each item runs creatTweetElment() then appends them to the tweetContainer 
   const renderTweets = function(arrayOfTweet) {
     const $tweetContainer = $('#tweets-container');
     for (let item of arrayOfTweet) {
       $tweetContainer.append(createTweetElement(item));
     }
-  
-  
   };
 
 
-  renderTweets(data);
+  const loadTweets = function(){
+    let url = 'http://localhost:8080/tweets';
+    $.ajax({
+      url: url,
+      method: "GET"
+    })
+    .then((result)=>{
+      //console.log('results', results);
+      renderTweets(result);
+    })
+    .catch((error)=>{
+      console.log('error: ', error)
+    });
+  };
 
-  //const $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-  //console.log($tweet); // to see what it looks like
-  //$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
-
+  loadTweets();
 
 });
